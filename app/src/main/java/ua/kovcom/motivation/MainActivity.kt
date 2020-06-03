e279ce358827cd91207d5faf92826518933d8ed9
mykhailo.kovalyk <mykhailo.kovalyk@eleks.com>
2020-06-04 02:26:53 +0300

    Initial commit. Add general app structure

---
+++ app/src/main/java/ua/kovcom/motivation/MainActivity.kt
@@ -0,0 +1,18 @@
+package ua.kovcom.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ua.kovcom.motivation.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
